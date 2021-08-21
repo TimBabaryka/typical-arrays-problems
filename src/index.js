5b7e19ee4b78efa074b0574b0d6425663324b886
Tim Babaryka <timasky95@gmail.com>
2021-08-21 19:44:43 +0300

    feat: tap

--- src/index.js
+++ src/index.js
@@ -1,11 +1,53 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+
+  if (array === 0 || array === undefined) {
+    return 0;
+  } else {
+
+  let mini = array[0];
+  
+    for (let i=0; i < array.length; i++) {
+      if(array[i] <= mini) {
+          min = (array[i])
+      } return mini;
+   }
+  }
+ }
+
+
+
+
+
+ 
+  
+   
+  
+
+
+
+
+
+
+
+
+
+
 
 exports.max = function max (array) {
   return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
 
 exports.avg = function avg (array) {
   return 0;
